b2cadb352d5b3a5a865f4ec54d36ff01cd9edc18
= <=>
2021-04-22 13:22:00 +0100

    Delete tasks. Style changes

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ function saveTask(e) {
     let taskTitle = document.getElementById('taskTitle').value;
     let taskText  = document.getElementById('taskText').value;
     const task = {
+        id: 0,
         title: taskTitle,
         text: taskText
     }
 
-    if(localStorage.getItem('taskList') === null) {
+    if(localStorage.getItem('taskList') === null ){
         let taskList = [];
         taskList.push(task);
         localStorage.setItem('taskList', JSON.stringify(taskList));
     }
     else {
         let taskList = JSON.parse(localStorage.getItem('taskList'));
+        if(taskList.length != 0) task.id = taskList[taskList.length-1].id + 1;
         taskList.push(task);
         localStorage.setItem('taskList', JSON.stringify(taskList));
     }
@@ -29,48 +31,13 @@ function printTasks() {
     let divContent = "";
     let j = 0;
     for (let i = 0; i < taskList.length; i+=2) {
-        console.log(taskList.length);
-        const task = taskList[i];
-        let title = task.title;
-        let desc = task.text;
         divContent = "";
         j = i + 1;
-
         if(j < taskList.length) {
             divContent += printFullRow(taskList, i);
-            /*`<div class="row pb-3">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-body">
-                        <div class="bg-warning w-100 h-100"><p class="text-center">${title}</p></div>
-                            <p>- ${desc} </p>
-                        </div>
-                    </div>
-                </div>`
-            title = taskList[j].title;
-            desc = taskList[j].text;
-            divContent += 
-                `<div class="col-6">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <div class="bg-warning w-100 h-100"><p class="text-center">${title}</p></div>
-                            <p>- ${desc} </p>
-                        </div>
-                    </div>
-                </div>`;*/
         }
         else {
             divContent += printTaskCol(taskList, i);
-            /*`<div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <div class="bg-warning w-100 h-100">
-                        <p class="text-center">${title}</p>
-                    </div>
-                    <p>- ${desc} </p>
-                </div>
-            </div>
-        </div>`*/
         }
         taskDiv.innerHTML += divContent;   
     }
@@ -97,12 +64,19 @@ function printTaskCol(taskList, i) {
                     <p>- ${taskList[i].text} </p>
                 </div>
                 <div class="options row">
-
+                    <button class="btn btn-danger" onClick="deleteTask(${i})">Delete</button>
                 </div>
             </div>
         </div>
     </div>`;
     return result;
+}
+
+function deleteTask(taskID) {
+    let taskList = JSON.parse(localStorage.getItem('taskList'));
+    taskList.splice(taskID, 1);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    printTasks();
 }
 
 printTasks();
